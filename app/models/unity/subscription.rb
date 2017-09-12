73119c65003c8cf002d9165e518ec1f44338e829
Keith Ward <trueinviso@gmail.com>
2017-09-11 21:53:31 -0700

    add user to dummy app

--- app/models/unity/subscription.rb
+++ app/models/unity/subscription.rb
@@ -11,39 +11,5 @@ module Unity
       pending: 4,
     }
 
-    scope :expiring, -> {
-      includes(:user)
-        .where(trial_ends_at: 1.day.from_now.beginning_of_day..2.days.from_now.end_of_day)
-    }
-
-    def days_until_expiration
-      return 0 unless trial_ends_at.present?
-      trial_ends_at.to_date.mjd - Time.zone.now.to_date.mjd
-    end
-
-    def trial_expired?
-      return true unless trial_ends_at.present?
-      trial_ends_at.end_of_day < Time.zone.today
-    end
-
-    def trial?
-      !trial_expired? && !subscribed?
-    end
-
-    def free?
-      trial_expired? && subscription_plan.free?
-    end
-
-    def premium_monthly?
-      subscription_plan.gateway_name == "premium_monthly_subscription"
-    end
-
-    def cancelled?
-      gateway_status == "canceled"
-    end
-
-    def subscribed?
-      gateway_id.present?
-    end
   end
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  mount Unity::Engine => "/unity"
+  mount Unity::Engine => "/subscription"
 end
