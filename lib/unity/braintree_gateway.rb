7aaaf72827dff61929e986fdbf90525d3dcbfdab
Keith Ward <trueinviso@gmail.com>
2017-10-15 11:06:19 -0700

    autoload files

--- lib/unity/braintree_gateway.rb
+++ lib/unity/braintree_gateway.rb
@@ -11,6 +11,9 @@ module Unity
     autoload :SubscriptionCanceller,"unity/braintree_gateway/subscription_canceller"
     autoload :SubscriptionUpdater,"unity/braintree_gateway/subscription_updater"
     autoload :UpdateSubscriptionPayload,"unity/braintree_gateway/update_subscription_payload"
+    autoload :DiscountHashBuilder,"unity/braintree_gateway/discount_hash_builder"
+    autoload :GetSwapDiscountPayload,"unity/braintree_gateway/get_swap_discount_payload"
+    autoload :SwapBillingCycle,"unity/braintree_gateway/swap_billing_cycle"
     autoload :Plan,"unity/braintree_gateway/plan"
     autoload :Actions,"unity/braintree_gateway/actions"
 
